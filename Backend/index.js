2719877d389ac891eee293dab2cd1e8796ae1104
FLCKE <louisfrancisco229@gmail.com>
2025-08-25 01:54:10 +0200

    serveless adaptation v1.6

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const app = require('./app'); // Assurez-vous que le chemin est correct
-import serverless from 'serverless-http';
+
 const db = require('./config/db'); // Assurez-vous que le chemin est correct
 require('dotenv').config();
 
@@ -13,4 +13,4 @@ db(); // Connexion à la base de données MongoDB
 app.listen(PORT, () => {
     console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
 });
-export default serverless(app);
+
