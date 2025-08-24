901d1304833bff2f3d6e6a7b84555786395381ae
FLCKE <louisfrancisco229@gmail.com>
2025-08-25 00:18:03 +0200

    serveless adaptation v1

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/ping', (req, res) => res.json({ pong: true }));
 
 
 db(); // Connexion à la base de données MongoDB
