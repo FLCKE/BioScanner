1296733b64bef66c5cbf78c823040158a71382e6
FLCKE <louisfrancisco229@gmail.com>
2025-09-01 01:38:54 +0200

    edit register redirection

--- Frontend/src/config/ProtectedRoutes.jsx
+++ Frontend/src/config/ProtectedRoutes.jsx
@@ -5,11 +5,8 @@ import { getUserPicture } from "../services/api";
 export default function ProtectedRoutes({ children }) {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  getUserPicture(localStorage.getItem("userId")).then((data) => {
-    console.log("data", data);
-    if (!data.imageUrl) {
-      navigate("/PhotoUpload");
-    }
+  getUserPicture(localStorage.getItem("userId")).catch((err) => {
+    navigate("/PhotoUpload");
   });
 
   return token ? children : <Navigate to="/login" />;
