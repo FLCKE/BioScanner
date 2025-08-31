1f51590c2298f3e9510f92cb2d31f3bc0ec3d58c
FLCKE <louisfrancisco229@gmail.com>
2025-09-01 01:45:36 +0200

    edit register redirection

--- Frontend/src/config/ProtectedRoutes.jsx
+++ Frontend/src/config/ProtectedRoutes.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { getUserPicture } from "../services/api";
+import { useEffect } from "react";
 
 export default function ProtectedRoutes({ children }) {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  getUserPicture(localStorage.getItem("userId")).catch((err) => {
-    navigate("/PhotoUpload");
-    return;
-  });
+  useEffect(() => {
+    if (token) {
+      getUserPicture(localStorage.getItem("userId")).catch(() => {
+        navigate("/PhotoUpload");
+      });
+    }
+  }, [token, navigate]);
 
   return token ? children : <Navigate to="/login" />;
 }
